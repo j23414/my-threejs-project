de06fee553757af8ea4ed8d97ad5762e7129005b
Jennifer Chang <jennifer.chang.bioinform@gmail.com>
2025-12-07 13:57:57 -0800

    Add classes for Sizes and Timer, turn on global access for ProteinViewer in console

--- src/ProteinViewer/ProteinViewer.js
+++ src/ProteinViewer/ProteinViewer.js
@@ -1,4 +1,6 @@
 import * as THREE from 'three'
+import Sizes from './Utils/Sizes.js'
+import Timer from './Utils/Timer.js'
 
 export default class ProteinViewer
 {
@@ -6,5 +8,28 @@ export default class ProteinViewer
   {
     console.log("Create ProteinViewer")
     this.canvas = canvas
+
+    // Allow access from Console, for debugging
+    window.proteinViewer = this
+
+    this.sizes = new Sizes()
+    this.timer = new Timer()
+
+    this.sizes.on('resize', () =>
+    {
+      this.resize()
+    })
+    this.timer.on('tick', () =>
+    {
+      this.update()
+    })
+  }
+  resize()
+  {
+    // Resize event
+  }
+  update()
+  {
+    // Update event, particularly for animations
   }
 }
